5bac644a50157301ac63a6e59e0539dcd7f5f1ed
Zakariye <0e6xrww7@students.codeinstitute.net>
2025-12-14 11:23:58 +0100

    Add toaster for messages

---
+++ frontend/layers/app/providers/ToastProvider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Toaster } from "react-hot-toast";
+
+
+export default function ToastProvider() {
+    return (
+        <Toaster
+            position="top-center"
+            toastOptions={{
+                duration: 5000,
+            }}
+        />
+    )
+}
