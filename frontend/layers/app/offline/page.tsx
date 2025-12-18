2575c7764b9ed9252a5bdbfd53349bd52cba9cef
Zakariye <0e6xrww7@students.codeinstitute.net>
2025-12-18 11:16:47 +0100

    Deployed frontend successfully to vercel

--- frontend/layers/app/offline/page.tsx
+++ frontend/layers/app/offline/page.tsx
@@ -9,7 +9,7 @@ export default function Offline() {
     <div className="flex flex-col min-h-screen">
       <div className="flex-1 flex items-center justify-center">
         <div className="text-center">
-          <h1>You're Offline</h1>
+          <h1 className='capitalize'>You're Offline</h1>
           <p>Please check your internet connection</p>
         </div>
       </div>
